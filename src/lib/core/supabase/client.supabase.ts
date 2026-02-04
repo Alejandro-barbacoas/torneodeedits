44cd023f0c6f1e656dbabf2d2a4e5a5c52c9e503
alejimfaj028 <alejimfaj028@gmail.com>
2026-02-04 20:35:07 +0000

    chore: configure variables de entorno de supabase

--- src/lib/core/supabase/client.supabase.ts
+++ src/lib/core/supabase/client.supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'TU_URL_DE_SUPABASE';
-const supabaseAnonKey = 'TU_ANON_KEY_DE_SUPABASE';
+const supabaseUrl = 'https://mbdczthbqyecpedargls.supabase.co';
+const supabaseAnonKey = 'sb_publishable_yiMiGFDFGq6RPPSfnNpq3A_ADaHBYOv';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
